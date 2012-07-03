169c564dba1e74d2a4c33674e23d244b923d5b8c
Alan Deitch <alandeitch@gmail.com>
2012-07-03 08:35:48 -0500

    setup rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transit::Application.config.secret_token = '8c0fdef7282c6066489a81ea0a161f05df92a31bd0eab0a9e66e03e3a079c1542174495b78030a71771ae2b3b31c1c129409ccfae099cfddd32c70f4eba456cd'
